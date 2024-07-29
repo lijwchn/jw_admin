36000377731e0f9585d77fd48cfbcc5f98f3f86c
lijwchn <3106218645@qq.com>
2024-07-29 15:25:21 +0800

    修改docker-compose文件，去除mysql容器

--- ninja_demo/settings/prod.py
+++ ninja_demo/settings/prod.py
@@ -16,8 +16,8 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "ninja_demo",  # 你的数据库名
         "USER": "root",  # 你的数据库用户名
-        "PASSWORD": "123456",  # 你的数据库密码
-        "HOST": "db",  # 数据库服务器地址，如果是本地则为localhost
+        "PASSWORD": "423432dfsafafdse%%",  # 你的数据库密码
+        "HOST": "172.16.16.14",  # 数据库服务器地址，如果是本地则为localhost
         "PORT": "3306",  # MySQL默认端口
         "POOL_OPTIONS": {
             "POOL_SIZE": 10,
