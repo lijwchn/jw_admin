a255414b07b884a9e4f81c1b76491d276cf5326f
lijwchn <3106218645@qq.com>
2024-07-29 17:16:57 +0800

    修改数据库配置防止明文

--- ninja_demo/settings/prod.py
+++ ninja_demo/settings/prod.py
@@ -12,14 +12,20 @@ ALLOWED_HOSTS = [
 # 允许所有来源都能访问，为了解决 cors 跨域问题
 CORS_ORIGIN_ALLOW_ALL = True
 
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "root")
+DB_HOST = os.getenv("DB_HOST", "db")
+DB_PORT = os.getenv("DB_PORT", 3306)
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("DB_NAME"),  # 你的数据库名
-        "USER": os.getenv("DB_USER", "root"),  # 你的数据库用户名
-        "PASSWORD": os.getenv("DB_PASSWORD"),  # 你的数据库密码
-        "HOST": os.getenv("DB_HOST"),  # 数据库服务器地址，如果是本地则为localhost
-        "PORT": os.getenv("DB_PORT"),  # MySQL默认端口
+        "NAME": DB_NAME,  # 你的数据库名
+        "USER": DB_USER,  # 你的数据库用户名
+        "PASSWORD": DB_PASSWORD,  # 你的数据库密码
+        "HOST": DB_HOST,  # 数据库服务器地址，如果是本地则为localhost
+        "PORT": DB_PORT,  # MySQL默认端口
         "POOL_OPTIONS": {
             "POOL_SIZE": 10,
             "MAX_OVERFLOW": 10,
