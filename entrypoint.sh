c3bee3c246cc891f6132286cc1b144fb22069397
lijwchn <3106218645@qq.com>
2024-07-29 15:41:35 +0800

    修改docker-compose文件

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-# 等待数据库服务启动并准备好连接
-while ! nc -z db 3306; do
-  echo "等待数据库服务启动..."
-  sleep 1
-done
-
 # 迁移数据库
 python manage.py migrate
 
 # 启动 Django 应用
-exec "$@"
+uvicorn ninja_demo.asgi:application --host 0.0.0.0 --port 8000 --reload --workers 5
